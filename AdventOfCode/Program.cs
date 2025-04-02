4034f92408d374d6a32bc09d1d424d59f95cdde0
Sanra22 <Brina3009@web.de>
2025-04-02 20:17:52 +0200

    Advent of Code Day 1

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    // Die Main-Methode, die als Einstiegspunkt für das Programm dient.
+    static void Main(string[] args)
+    {
+        int[] left;
+        int[] right;
+
+        string[] lines = File.ReadAllLines("Input.txt");
+
+        left = new int[lines.Length];
+        right = new int[lines.Length];
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string[] parts = lines[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            left[i] = int.Parse(parts[0]);   // Erste Zahl ins 'left'-Array
+            right[i] = int.Parse(parts[1]);  // Zweite Zahl ins 'right'-Array
+        }
+
+        int z = 0;
+        int y = 0;
+
+        Array.Sort(left);
+        Array.Sort(right);
+        for(int i = 0; i < left.Length; i++) 
+        {
+            z = Math.Abs(left[i] - right[i]);
+            y = y +z;
+        }
+        Console.WriteLine(y);
+        Console.ReadKey();
+    }
+}
--- AdventOfCode/obj/Debug/net8.0/AdventOfCode.AssemblyInfo.cs
+++ AdventOfCode/obj/Debug/net8.0/AdventOfCode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c17da8c336e27d232bd00ac7e7601864f2a4b963")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
